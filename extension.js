5080c3429dd7a01470b4e63a15e4c280fcd0a4a1
domgregori <dom.gregori@gmail.com>
2024-05-22 16:46:26 -0400

    Changed code to meet requirements
    Moved sections of code to meet requirements for gnome extenion upload

--- extension.js
+++ extension.js
@@ -2,35 +2,33 @@ import GObject from 'gi://GObject';
 import St from 'gi://St';
 import Meta from 'gi://Meta';
 
-import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
-const Indicator = GObject.registerClass(
-class Indicator extends PanelMenu.Button {
-    _init() {
-        super._init(0.0, _('Fullscreen Button'));
-
-        this.add_child(new St.Icon({
-            icon_name: 'view-fullscreen-symbolic',
-            style_class: 'system-status-icon',
-        }));
-    }
-});
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 
 export default class FullscreenButtonExtension extends Extension {
+    constructor(metadata) {
+        super(metadata);
+    }
     enable() {
-        this._indicator = new Indicator();
+        this._indicator = new PanelMenu.Button(0.0, this.metadata.name, false);
+        const icon = new St.Icon({
+            icon_name: 'view-fullscreen-symbolic',
+            style_class: 'system-status-icon',
+        });
+        this._indicator.add_child(icon);
+
         Main.panel.addToStatusArea(this.uuid, this._indicator);
         this._indicator.connect('touch-event', this._buttonActivated);
         this._indicator.connect('button-press-event', this._buttonActivated);
     }
 
     disable() {
-        this._indicator.destroy();
+        this._indicator?.destroy();
         this._indicator = null;
     }
+
     _buttonActivated(actor, event) {
         let activeWindow = global.display.focus_window;
         if (activeWindow) {
