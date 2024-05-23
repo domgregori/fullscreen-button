9117802c9b0a293dfa4b165bfa1be7d5f0af94f0
domgregori <dom.gregori@gmail.com>
2024-05-23 12:06:57 -0400

    Changed code for gnome v41-44

--- extension.js
+++ extension.js
@@ -1,47 +1,62 @@
-import St from 'gi://St';
+const Gettext = imports.gettext;
+const St = imports.gi.St;
 
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
 
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 
-export default class FullscreenButtonExtension extends Extension {
-    constructor(metadata) {
-        super(metadata);
+const {
+  gettext: _,
+  ngettext,
+  pgettext,
+} = ExtensionUtils;
+
+class Extension {
+  enable() {
+      // Create a panel button
+      this._indicator = new PanelMenu.Button(0.0, Me.metadata.name, false);
+
+      // Add an icon
+      const icon = new St.Icon({
+          icon_name: 'view-fullscreen-symbolic',
+          style_class: 'system-status-icon',
+      });
+      this._indicator.add_child(icon);
+
+      Main.panel.addToStatusArea(Me.metadata.uuid, this._indicator);
+      this._tapHandler = this._indicator.connect('touch-event', this._buttonActivated);
+      this._clickHandler = this._indicator.connect('button-press-event', this._buttonActivated);
+  }
+
+  disable() {
+     if (this._tapHandler) {
+          this._indicator.disconnect(this._tapHandler);
+          this._tapHandler = null;
+      }
+      if (this._clickHandler) {
+          this._indicator.disconnect(this._clickHandler);
+          this._clickHandler = null;
+      }
+      if (this._indicator) {
+          this._indicator.destroy();
+          this._indicator = null;
+      }
+  }
+
+  _buttonActivated() {
+    let activeWindow = global.display.focus_window;
+    if (activeWindow) {
+        activeWindow.make_fullscreen();
     }
-    enable() {
-        this._indicator = new PanelMenu.Button(0.0, this.metadata.name, false);
-        const icon = new St.Icon({
-            icon_name: 'view-fullscreen-symbolic',
-            style_class: 'system-status-icon',
-        });
-        this._indicator.add_child(icon);
-
-        Main.panel.addToStatusArea(this.uuid, this._indicator);
-        this._tapHandler = this._indicator.connect('touch-event', this._buttonActivated);
-        this._clickHandler = this._indicator.connect('button-press-event', this._buttonActivated);
-    }
-
-    disable() {
-        if (this._tapHandler) {
-            this._indicator.disconnect(this._tapHandler);
-            this._tapHandler = null;
-        }
-        if (this._clickHandler) {
-            this._indicator.disconnect(this._clickHandler);
-            this._clickHandler = null;
-        }
-        this._indicator?.destroy();
-        this._indicator = null;
-    }
-
-    _buttonActivated() {
-        let activeWindow = global.display.focus_window;
-        if (activeWindow) {
-            activeWindow.make_fullscreen();
-        }
-    }
-    
+  }
 }
 
+function init() {
+  // If the `gettext-domain` key is not set in `metadata.json`, you must
+  // pass the unique Gettext domain for your extension when initializing.
+  ExtensionUtils.initTranslations(Me.metadata.uuid);
 
+  return new Extension();
+}
